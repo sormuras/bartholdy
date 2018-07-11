85fb3f5dc6494aec982d4804f34263ccf1c10bff
Christian Stein <sormuras@gmail.com>
2018-07-11 13:47:28 +0200

    Add Gradle Wrapper test

--- src/test/java/integration/tool/GradleTests.java
+++ src/test/java/integration/tool/GradleTests.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import de.sormuras.bartholdy.AbstractTool;
+import de.sormuras.bartholdy.Bartholdy;
 import de.sormuras.bartholdy.Configuration;
+import de.sormuras.bartholdy.Tool;
 import de.sormuras.bartholdy.tool.Gradle;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Locale;
 import org.junit.jupiter.api.Test;
 
 class GradleTests {
@@ -29,6 +34,10 @@ class GradleTests {
     assertEquals("gradle", tool.getName());
     assertTrue(Files.exists(tool.getHome()));
 
+    version(version, tool);
+  }
+
+  private void version(String version, Tool tool) {
     var result = tool.run(Configuration.of("-version", "--no-daemon"));
     assertEquals(0, result.getExitCode());
     // assertEquals("", result.getOutput("err")); // Illegal Access is reported here
@@ -47,5 +56,37 @@ class GradleTests {
             "JVM:          .+",
             "OS:           .+");
     assertLinesMatch(expectedLines, result.getOutputLines("out"));
+  }
+
+  @Test
+  void gradleWrapper() {
+    var tool = new GradleWrapper();
+    version(tool.getVersion(), tool);
+  }
+
+  class GradleWrapper extends AbstractTool {
+
+    @Override
+    protected Path createPathToProgram() {
+      return getHome().resolve(getProgram());
+    }
+
+    @Override
+    public String getName() {
+      return "gradle-wrapper";
+    }
+
+    @Override
+    public String getProgram() {
+      var win = System.getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("win");
+      return "gradlew" + (win ? ".bat" : "");
+    }
+
+    @Override
+    public String getVersion() {
+      var jar = getHome().resolve("gradle").resolve("wrapper").resolve("gradle-wrapper.jar");
+      var text = Bartholdy.read(jar, "/build-receipt.properties", System.lineSeparator(), "?");
+      return Bartholdy.readProperty(text, "versionNumber", "unknown");
+    }
   }
 }
